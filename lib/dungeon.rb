409c8c990e60e8c2d2786abece48ab23aeef6b7f
ced <zuger.cedric@gmail.com>
2018-03-12 11:29:33 +0100

    Rooms on map.

--- lib/dungeon.rb
+++ lib/dungeon.rb
@@ -1,8 +1,10 @@
 require_relative 'room'
+require 'rmagick'
 
 class Dungeon
 
   def initialize( dungeon_size )
+    @dungeon_size = dungeon_size
     @rooms = {}
     1.upto(dungeon_size) do |top|
       1.upto(dungeon_size) do |left|
@@ -11,6 +13,24 @@ class Dungeon
     end
   end
 
+  def draw
+    width = height = ( @dungeon_size * Room::ROOM_SQUARE_SIZE +
+        ( ( @dungeon_size-1 ) * Room::SQUARES_BETWEEN_ROOMS ) ) * Room::SQUARE_SIZE_IN_PIXELS +
+        ( Room::ROOM_SQUARE_SIZE * Room::SQUARE_SIZE_IN_PIXELS )
+
+    canvas = Magick::Image.new( width, height )
+
+    gc = Magick::Draw.new
+    gc.stroke( 'darkslateblue' )
+    gc.fill( 'white' )
+
+    @rooms.each_pair{ |_, r| r.draw( gc ) }
+
+    gc.draw( canvas )
+    canvas.write( 'out/dungeon.jpg' )
+
+  end
+
 end
 
-Dungeon.new( 4 )
+Dungeon.new( 5 ).draw
